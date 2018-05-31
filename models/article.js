c3262ade8422dc7987a7e303a147e76b255633a2
Wesley Hanson <wrh1600@gmail.com>
2018-05-30 23:12:51 -0700

    creating simple blog app

---
+++ models/article.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+// Article Schema
+
+let articleSchema = mongoose.Schema({
+    title:{
+        type: String,
+        required: true,
+    },
+    author:{
+        type: String,
+        required: true
+    },
+    body:{
+        type: String,
+        required: true
+    }
+});
+
+
+const Article = module.exports = mongoose.model('Article', articleSchema);
---
+++ server.js
@@ -0,0 +1,80 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+mongoose.connect('mongodb://localhost/nodedb');
+let db = mongoose.connection;
+
+//Check connection
+db.once('open', function(){
+    console.log('Connected to MongoDB');
+})
+
+//Check for db errors
+db.on('error', function(err){
+    console.log(err);
+})
+
+// Init App
+const app = express();
+
+//Bring in models
+const Article = require('./models/article');
+
+// Load View Engine
+app.set('/views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+// Home Route
+app.get('/', function(req, res){
+    Article.find({}, function(err, articles){
+        if(err){
+            console.log(err);
+        }
+        else{
+            res.render('index', {
+                title:'Articles',
+                articles: articles
+            });
+        }
+        
+    })
+    
+});
+
+app.get('/articles/add', function(req, res){
+    res.render('add_article', {
+        title: 'Add Articles'
+    })
+})
+
+// Add Submit POST Route
+app.post('/articles/add', function(req, res){
+   let article = new Article();
+   article.title = req.body.title;
+   article.author = req.body.author;
+   article.body = req.body.body;
+
+   article.save(function(err){
+       if(err){
+           console.log(err);
+           return;
+       }
+       else{
+           res.redirect('/');
+       }
+   })
+})
+
+
+// Start Server
+app.listen(3000, function(){
+    console.log('Server started on port 3000');
+});
